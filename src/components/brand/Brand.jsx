beb7616c4e4bfadc7ce30037b7bdcf8169b2f295
Khamzat91 <khamzat91@mail.ru>
2022-08-05 23:10:04 +0300

    сделал html разметку

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { brandCards } from './brendProduct';
+import { ReactComponent as BrandArrow } from '../../images/content/brand-arrow.svg';
+import "./index.scss";
+
+export const Brand = () => {
+  return (
+    <div className='brand'>
+     <div className="brand__title">Бренды</div>
+     {
+        brandCards.map((obj) =>  <div className="brand-ad">
+        <div className="brand-ad__image">{obj.linkBrandImg}</div>
+        <div className="brand-ad__title">{obj.title}</div>
+     </div>)
+     }
+     <div className="brand__all">
+        <button>Смотреть все бренды</button>
+        <BrandArrow/>
+     </div>
+    
+    </div>
+  )
+}
+
+
+export default Brand;
---
+++ src/components/brand/brendProduct.js
@@ -0,0 +1,66 @@
+import brand1 from "../../images/brand1.png";
+import brand2 from "../../images/brand2.png";
+import brand3 from "../../images/brand3.png";
+import brand4 from "../../images/brand4.png";
+import brand5 from "../../images/brand5.png";
+import brand6 from "../../images/brand6.png";
+import brand7 from "../../images/brand7.png";
+import brand8 from "../../images/brand8.png";
+import brand9 from "../../images/brand9.png";
+import brand10 from "../../images/brand10.png";
+import brand11 from "../../images/brand11.png";
+import brand12 from "../../images/brand12.png";
+
+
+
+
+export const brandCards = [
+    {
+        linkBrendImg: brand1,
+        title: 'Philips Avent'
+    },
+    {
+        linkBrandImg: brand2,
+        title: 'Hipp'
+    },
+    {
+        linkBrandImg: brand3,
+        title: 'Omron'
+    },
+    {
+        linkBrandImg: brand4,
+        title: 'NUK'
+    },
+    {
+        linkBrandImg: brand5,
+        title: 'Nutrilon'
+    },
+    {
+        linkBrandImg: brand6,
+        title: 'Britax Romer'
+    },
+    {
+        linkBrandImg: brand7,
+        title: 'Puky'
+    },
+    {
+        linkBrandImg: brand8,
+        title: 'The name of Brand'
+    },
+    {
+        linkBrandImg: brand9,
+        title: 'Ergobaby'
+    },
+    {
+        linkBrandImg: brand10,
+        title: 'Alvi'
+    },
+    {
+        linkBrandImg: brand11,
+        title: 'Baby Born'
+    },
+    {
+        linkBrandImg: brand12,
+        title: 'Chicco'
+    }
+]
