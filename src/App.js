5d2ce033c7bef89d804b49ac9ebaa5fd32cdca07
Khamzat91 <khamzat91@mail.ru>
2022-08-10 00:04:25 +0300

    added reviews

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Novelty from "./components/novelty/Novelty";
 import AboutUs from "./components/aboutUs/AboutUs";
 import Brand from "./components/brand/Brand";
 import Contacts from "./components/contacts/Contacts";
+import Reviews from "./components/reviews/Reviews";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
       <AboutUs/>
       <Brand/>
       <Contacts/>
+      <Reviews/>
       </Container>
     </div>
   );
---
+++ src/components/reviews/Reviews.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { customerCard } from "./shopper";
+import { ReactComponent as Google } from "../../images/content/google.svg";
+import "./index.scss";
+
+export const Reviews = () => {
+    const settings = {
+       
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
+  return (
+    <div className="reviews">
+      <div className="reviews__title">Отзывы покупателей</div>
+      <div className="reviews__result">
+        Средняя оценка на Google: <span>4,9</span> из 5
+      </div>
+      <Slider {...settings}>
+        {customerCard.map((obj) => (
+          <div className="reviews__card">
+            <div className="reviews__card-data">
+              {<div className="reviews__card-avatar">{obj.avatar ? <img src="" alt=""/>: obj.name[0]}</div>}
+
+              <div className="reviews__card-info">
+                <div className="reviews__card-name">{obj.name}</div>
+                <div className="reviews__card-box">
+                 {obj.technician &&  <div className="reviews__card-technician">
+                    {obj.technician} .
+                  </div>}
+                  <div className="reviews__card-feedback">
+                    {obj.feedback} отзыв .
+                  </div>
+                  <div className="reviews__card-count">{obj.countImg} фото</div>
+                </div>
+              </div>
+            </div>
+            <div className="reviews__card-recency">
+              <div className="reviews__card-rating">{obj.rating}</div>
+              <div className="reviews__card-age">{obj.age} года назад</div>
+            </div>
+            <div className="reviews__card-text">{obj.text}</div>
+            <div className="reviews__card-img">
+              <img src={obj.like} alt="" className="reviews__card-like" />
+              <img src={obj.share} alt="" className="reviews__card-share" />
+            </div>
+            <div className="reviews__card-option">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      <a className="reviews__feedback-all">
+        <Google className="reviews__feedback-all__google"/>
+        Смотреть все отзывы Google
+        </a>
+    </div>
+  );
+};
+
+export default Reviews;
